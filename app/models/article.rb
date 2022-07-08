d84394cd5570266c0a8243fc5c0a7f8af19ab325
中原良太 <>
2022-07-09 00:15:52 +0900

    DAy19-2

--- app/models/article.rb
+++ app/models/article.rb
@@ -19,13 +19,13 @@ class Article < ApplicationRecord
   validates :title, format: { with: /\A(?!\@)/ }
   validate :validate_title_and_content_length
 
+  def display_created_at
+    I18n.l(self.created_at, format: :default)
+  end
+
   private
   def validate_title_and_content_length
     char_count = self.title.length + self.content.length
     errors.add(:content, '100文字いじょうで！') unless char_count > 100
-  end
-
-  def display_created_at
-    I18n.l(self.created_at, format: :default)
   end
 end
